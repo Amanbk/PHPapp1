12aa1311d1b4987850e3437a71c76c7450d3c9d9
Amanbk <amanbereket9@gmail.com>
2018-03-24 21:27:22 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,8 +3,12 @@
 $firstname= $_POST["firstname"];
 $lastname= $_POST["lastname"];
 
+if ((!empty(firstname))&& (&empty(lastname))){
+  echo $firstname;
+}else{
+  echo "please fill out the forms";
+}
 
 
-echo $username;
 
 ?>
