523d6987bf5b83fd1d10822cd2925e30bf5d3e42
Amanbk <amanbereket9@gmail.com>
2018-03-24 21:45:31 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,13 +2,58 @@
 
 $firstname= $_POST["firstname"];
 $lastname= $_POST["lastname"];
+$reply;
 
-if ((!empty(firstname))&& (&empty(lastname))){
-  echo $firstname;
+$conn = mysqli_connect($server, $username, $password);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+if ((!empty(firstname))&& (!empty(lastname))){
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    //1 creating database
+    $sql = "CREATE DATABASE phpapp1";
+    if (mysqli_query($conn, $sql)) {
+      
+    }else {
+        $reply= "Error : " . mysqli_error($conn);
+    }
+
+  
+    //2 creating table
+    $sql2 = "CREATE TABLE users (
+    id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    )";
+
+    if ($conn->query($sql2) === TRUE) {
+      
+    } else {
+        $reply= "Error : " . $conn->error;
+    }
+  
+    //3 inserting data
+  
+    $sql3 = "INSERT INTO users (firstname, lastname)
+    VALUES ('$firstname', '$lastname')";
+
+    if ($conn->query($sql) === TRUE) {
+       $reply= $firstname;
+    } else {
+        $reply= "Error: " . $conn->error;
+    }
+  
+   echo $reply;
 }else{
   echo "please fill out the forms";
 }
 
 
 
+mysqli_close($conn);
 ?>
